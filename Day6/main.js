64348baf9610ff419dfd5cb6ea65012ccfa5329b
TienDuong <duongb2012073@gmail.com>
2024-07-23 20:34:55 +0700

    Day6

--- Day6/main.js
+++ Day6/main.js
@@ -4,12 +4,15 @@ let eWhich  = document.querySelector('.card.which p:last-child');
 let eCode  = document.querySelector('.card.code p:last-child');
 let alert = document.querySelector('.alert');
 let box = document.querySelector('.box');
+let result = document.querySelector('.result');
 
 document.addEventListener('keydown', e => {
     eKey.innerText = e.key;
     eLocation.innerText = e.location;
     eWhich.innerText = e.which;
     eCode.innerText = e.code;
+    result.innerText = e.code;
+
     alert.classList.add('hide');
     box.classList.remove('hide');
 })
